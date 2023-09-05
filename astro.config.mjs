272f0063c7a277ab1e56422e3de0359c9a3a5ea5
z2labplus <858570636@qq.com>
2023-09-05 14:11:20 +0800

    build1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     base:"/astrodemo",
     buildOptions: {
+        site: 'https://liveup7.github.io', // 你的网站URL
         base: '/astrodemo/',
     },
 });
